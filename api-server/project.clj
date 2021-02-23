6cb85363481cc01f0f247799957a45d45a499d37
iku000888 <kanumaiku@gmail.com>
2021-02-23 01:01:18 -0800

    api

--- api-server/project.clj
+++ api-server/project.clj
@@ -1,3 +1,4 @@
 (defproject api-server "0.1.0"
+  :repl-options {:init-ns ser.ver}
   :dependencies [[org.clojure/clojure "1.10.2"]
-                 [ring/ring-core "1.9.1"]])
+                 [ring/ring "1.9.1"]])
--- api-server/src/ser/ver.clj
+++ api-server/src/ser/ver.clj
@@ -1,1 +1,13 @@
-(ns ser.ver)
+(ns ser.ver
+  (:require [ring.adapter.jetty :as j]))
+
+(defn serve []
+  (let [s (atom 0)]
+    (j/run-jetty
+     (fn [req]
+       {:status 200
+        :body (str (swap! s inc))})
+     {:join? false
+      :port 3000})))
+(comment
+  (def s (serve)))
